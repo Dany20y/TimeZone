4935a0a50926de73c4f3b51154ab182742eb0f4b
Cuflic Dan <daniel.cuflic12@gmail.com>
2024-02-20 21:50:14 +0200

    ProductDetails

--- TimeZone/Controllers/HomeController.cs
+++ TimeZone/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace TimeZone.Controllers
         {
             return View();
         }
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
     }
 }
