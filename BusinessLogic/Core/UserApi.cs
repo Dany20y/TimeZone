49f5f9b7393a72d6400da47c5b3ad11d77a25e81
DomnulGosha <cioresco.adrian@gmail.com>
2024-03-05 20:42:25 +0200

    BL

--- BusinessLogic/Core/UserApi.cs
+++ BusinessLogic/Core/UserApi.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeZone.Domain.Entities.User;
+using TimeZone.Domain.Entities.Res;
+using TimeZone.Domain.Entities.User.Global;
+using TimeZone.Domain.Entities.Porduct;
+using System.Net.Http.Headers;
 
-namespace BusinessLogic.Core
+
+
+namespace TimeZone.BusinessLogic.Core
 {
-    internal class UserApi
+    public class UserApi
     {
+        internal ActionStatus UserLogData(ULoginData data)
+        {
+            return new ActionStatus();
+        }
+        
+        internal LevelStatus CheckLevelLogic(string keySession)
+        {
+            return new LevelStatus();
+        }
+
+        internal ProductDetail GetProductUser(int id) 
+        {
+            return new ProductDetail();
+        }
     }
 }
---
+++ BusinessLogic/interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeZone.Domain.Entities.Product;
+
+namespace TimeZone.BusinessLogic.Interfaces
+{
+    public interface IProduct
+    {
+        ProductDetail GetDetailProduct(int id);
+    }
+}
