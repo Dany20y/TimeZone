d6653505810da73191ec64f93e1d28a2af238716
DomnulGosha <cioresco.adrian@gmail.com>
2024-03-05 21:55:04 +0200

    controller

---
+++ TimeZone/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLogic.Interfaces;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using TimeZone.BusinessLogic;
+
+namespace TimeZone.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ISession _session;
+        public LoginController() 
+        {
+            var bl = new BussinesLogic();
+            _session = bl.GetSessionBL();
+        }
+        // GET: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(userLogin login)
+        {   
+            if(ModelState.IsValid)
+            {
+                ULoginData data = new ULoginData
+                {
+                    CredentialCache = login.Credential,
+                    Password = login.Password,
+                    LoginIp = Request.UserHostAddress,
+                    LoginDataTime = DateTime.Now
+                };
+
+                var UserLogin = _session.UserLogin(data);
+                if (UserLogin.Status) 
+                {
+                    LevelStatus status = _session.CheckLevel(UserLogin.SessionKey);
+                    
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("Nume de utilizator sau parola incorecta. Va rugam sa incercati din nou!", UserLogin.StatusMessage);
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ TimeZone/Controllers/ProductDetailController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeZone.BusinessLogic;
+
+namespace TimeZone.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        private IProduct _product;
+        // GET: ProductDetail
+        ProductDetailController() 
+        { 
+            BussinesLogic bussinesl = new BussinesLogic();
+
+            _product = bussinesl.GetProductBL();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetProduct(int id)
+        {
+            ProductDetail productDetail = _product.GetProductBL(id);
+            return View();
+        }
+    }
+}
