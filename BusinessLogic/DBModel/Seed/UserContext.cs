8826087cd25363e6671336cc48dd35c4f022ae8d
DomnulGosha <cioresco.adrian@gmail.com>
2024-04-21 17:01:06 +0300

    Abac

---
+++ BusinessLogic/DBModel/Seed/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DBModel.Seed
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+        base("name = App")
+        {
+        }
+
+        public virtual DbSet<UDbTable> Users { get; set; }
+    }
+}
